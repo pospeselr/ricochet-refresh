b4808a0cbb9ace173a0ec54a809c1afb3d14ca64
Richard Pospesel <richard@blueprintforfreespeech.net>
2021-03-07 14:48:52 +0100

    libtego_ui: removed deprecated function call and enabled more warning build flags

--- src/libtego_ui/ui/MainWindow.cpp
+++ src/libtego_ui/ui/MainWindow.cpp
@@ -71,12 +71,9 @@ public:
     BlockedNetworkAccessManager(QObject *parent)
         : QNetworkAccessManager(parent)
     {
-        /* Either of these is sufficient to cause any network request to fail.
-         * Both of them should be redundant, because createRequest below also
+        /* This will cause any network request to fail.
+         * This should be redundant, because createRequest below also
          * blackholes every request (and crashes for assert builds). */
-
-        /* XXX: setNetworkAccessible is deprecated */
-        setNetworkAccessible(QNetworkAccessManager::NotAccessible);
         setProxy(QNetworkProxy(QNetworkProxy::Socks5Proxy, QLatin1String("0.0.0.0"), 0));
     }
 
